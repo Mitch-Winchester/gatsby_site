88b05738d81ea77b897a141b08445e57d903c29f
Mitch-Winchester <mitch.winchester91@outlook.com>
2024-09-26 10:23:07 -0400

    Fixed issue with long titles not centering correctly.

--- src/templates/recipe.js
+++ src/templates/recipe.js
@@ -3,13 +3,13 @@ import Layout from '../components/layout'
 
 const Recipe = ({ pageContext }) => {
     const { recipe } = pageContext
+
     return (
         <Layout pageTitle={recipe.title}>
             <div>
-                <p>Title: {recipe.title}</p>
                 <p>Difficulty: {recipe.difficulty}</p>
-                <p>Preparation Time: {recipe.preparationTime}</p>
-                <p>Cooking Time: {recipe.cookingTime}</p>
+                <p>Preparation Time: {recipe.preparationTime} minutes</p>
+                <p>Cooking Time: {recipe.cookingTime} minutes</p>
                 <p>Number of Servings: {recipe.numberOfServings}</p>
                 <p>Ingredients: {recipe.ingredients}</p>
             </div>
