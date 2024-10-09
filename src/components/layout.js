8465a8a18235273abaf14326a286a4762e1df849
Mitch-Winchester <mitch.winchester91@outlook.com>
2024-10-09 14:00:29 -0400

    Update navBar and recipe page with bootstrap.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,37 @@
 import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
 import { 
-    container,
+    myContainer,
     heading,
-    navLinks,
-    navLinkItem,
-    navLinkText,
     siteTitle,
-    body
- } from './layout.module.css'
+    myBody
+} from './layout.module.css'
+
+const NavList = styled.ul`
+    display: flex;
+    justify-content: center;
+    list-style: none;
+    padding: 0;
+    margin: auto auto 1rem;
+
+    li {
+        margin-bottom: 0.5rem;
+    }
+`;
+const NavLink = styled(Link)`
+    color: black;
+    padding: 0.5rem 1rem;
+    text-decoration: none;
+    border-radius: 0.25rem;
+    margin: 0 0.25rem;
+
+    &.active {
+        background-color: rebeccapurple;
+        color: white;
+    }
+`
+
 
 const Layout = ({ pageTitle, children }) => {
     const data = useStaticQuery(graphql`
@@ -22,44 +45,56 @@ const Layout = ({ pageTitle, children }) => {
     `)
 
     return (
-        <div className={body}>
-            <div className={container}>
+        <div className={myBody}>
+            <div className={myContainer}>
                 <header className={siteTitle}>{data.site.siteMetadata.title}</header>
                 <nav>
-                    <ul className={navLinks}>
-                        <li className={navLinkItem}>
-                            <Link to="/" className={navLinkText}>
+                    <NavList className="nav nav-pills">
+                        <li className="nav-item">
+                            <NavLink to="/"
+                                activeClassName="active"
+                            >
                                 Home
-                            </Link>
+                            </NavLink>
                         </li>
-                        <li className={navLinkItem}>
-                            <Link to="/about" className={navLinkText}>
+                        <li className="nav-item">
+                            <NavLink to="/about"
+                                activeClassName="active"
+                            >
                                 About
-                            </Link>
+                            </NavLink>
                         </li>
-                        <li className={navLinkItem}>
-                            <Link to="/blog" className={navLinkText}>
+                        <li className="nav-item">
+                            <NavLink to="/blog"
+                                activeClassName="active"
+                            >
                                 Blog
-                            </Link>
+                            </NavLink>
                         </li>
-                        <li className={navLinkItem}>
-                            <Link to="/recipes" className={navLinkText}>
+                        <li className="nav-item">
+                            <NavLink to="/recipes"
+                                activeClassName="active"
+                            >
                                 Recipes
-                            </Link>
+                            </NavLink>
                         </li>
-                        <li className={navLinkItem}>
-                            <Link to="/articles" className={navLinkText}>
+                        <li className="nav-item">
+                            <NavLink to="/articles"
+                                activeClassName="active"
+                            >
                                 Articles
-                            </Link>
+                            </NavLink>
                         </li>
-                    </ul>
-                    <ul className={navLinks}>
-                        <li className={navLinkItem}>
-                            <Link to="/val_comp" className={navLinkText}>
+                    </NavList>
+                    <NavList className="nav nav-pills">
+                        <li className="nav-item">
+                            <NavLink to="/val_comp"
+                                activeClassName="active"
+                            >
                                 Valheim Companion App
-                            </Link>
+                            </NavLink>
                         </li>
-                    </ul>
+                    </NavList>
                 </nav>
                 <main>
                     <h1 className={heading}>{pageTitle}</h1>
--- src/templates/recipe.js
+++ src/templates/recipe.js
@@ -1,39 +1,55 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { Row, Col } from 'react-bootstrap'
 
 const Recipe = ({ pageContext }) => {
     const { recipe } = pageContext
 
     return (
         <Layout pageTitle={recipe.title}>
-            <div className="row mb-4">
-                <div className="col-md-6 mb-3">
+            <Row>
+                <Col xs={12} md={6}>
                     <img 
                         src={recipe.mediaImage.mediaImage.url} 
                         alt={recipe.title}
                         className="img-fluid"
                     />
-                </div>
-                <div className="col-md-4">
-                    <p className="mb-2"><strong>Difficulty:</strong> {recipe.difficulty}</p>
-                    <p className="mb-2"><strong>Preparation Time:</strong> {recipe.preparationTime} minutes</p>
-                    <p className="mb-2"><strong>Cooking Time:</strong> {recipe.cookingTime} minutes</p>
-                    <p className="mb-2"><strong>Number of Servings:</strong> {recipe.numberOfServings}</p>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-md-4 mb-3">
+                </Col>
+                <Col xs={12} md={6}>
+                    <Row>
+                        <Col>
+                            <p><strong>Difficulty:</strong></p>
+                            <p>{recipe.difficulty}</p>
+                        </Col>
+                        <Col>
+                            <p><strong>Preparation Time:</strong></p>
+                            <p>{recipe.preparationTime} minutes</p>    
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <p><strong>Cooking Time:</strong></p>
+                            <p>{recipe.cookingTime} minutes</p>    
+                        </Col>
+                        <Col>
+                            <p><strong>Number of Servings:</strong></p>
+                            <p>{recipe.numberOfServings}</p>    
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12} md={4} className="mb-3">
                     <h2>Ingredients:</h2>
                     {recipe.ingredients?.map((ingredient, ingIndex) => (
                         <p key={ingIndex}>{ingredient}</p>
                     ))}
-                </div>
-                <div className="col-md-8">
+                </Col>
+                <Col xs={12} md={8}>
                     <h2>Recipe Instructions:</h2>
                     <p dangerouslySetInnerHTML={{ __html: recipe.recipeInstruction?.processed }}></p>
-                </div>
-            </div>
+                </Col>
+            </Row>
         </Layout>
     )
 }
